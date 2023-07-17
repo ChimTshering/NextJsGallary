7c682aaa5b58be01a8585f3d1b70d3111aef63cb
chimi <chimitshering505@gmail.com>
2023-07-18 00:18:50 +0600

    heater

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,7 @@ export default function Head() {
   return (
     <>
       <title>NextJs</title>
+      header
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
